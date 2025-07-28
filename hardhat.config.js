0ca3fcc8013316ddd247156be4721969e5c27ef8
Gérald Quenum <geraldquenum9@gmail.com>
2025-07-28 15:41:35 -0700

    init

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      // You can set chainId here if needed
+    },
+    // Add other networks as needed
+  },
+};
